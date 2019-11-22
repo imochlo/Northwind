50faea5180db7dd877f011f0d918b109e0393f2d
Sam Solis <e.jsbsolis@gmail.com>
2019-11-22 16:43:06 +0800

    fixed class

--- ETL.py
+++ ETL.py
@@ -1,10 +1,10 @@
-import sqlalchemy as db
 from sqlalchemy import Table
-import pandas as pd
 import logging
-import sqlite3
 import os.path
+import pandas as pd
 import re
+import sqlalchemy as db
+import sqlite3
 import sys
 
 LOG_FORMAT = "%(asctime)s ETL_Log[%(lineno)d] %(levelname)s: %(message)s"
@@ -21,15 +21,14 @@ class Db:
         self.metadata = db.MetaData(self.engine)
         self.url = str(self.engine.url)
 
-    def create_table(self, prefix, src, if_exists=True):
-        name = "_".join([prefix, src.name])
-        table = Table(name, self.metadata)
-        if name in self.tables:
+    def create_table(self, table, src, if_exists=True):
+        logger.debug("Creating table {} from {}".format(table.name, src.name))
+        if table.name in self.tables:
             if if_exists:
-                logger.warning("Dropping {} table from {}".format(name, self.url))
+                logger.warning("Dropping {} table from {}".format(table.name, self.url))
                 table.drop(self.engine)
             else:
-                logger.warning("Dropping {} table from {}".format(name, self.url))
+                logger.warning("Dropping {} table from {}".format(table.name, self.url))
                 return table
         for column in src.columns:
             logger.debug("""
@@ -40,7 +39,7 @@ class Db:
                         nullable:{nullable},
                         foreign_keys:{foreign_keys}]
                         """
-                        .format(table_name=name,
+                        .format(table_name=table.name,
                                 column=column.name,
                                 _type=column.type,
                                 primary_key=column.primary_key,
@@ -54,6 +53,15 @@ class Db:
     @property
     def tables(self):
         return self.engine.table_names()
+
+class ETL_Table:
+    def __init__ (self, prefix, src, db, if_exists=True):
+        name = "_".join([prefix, src.name])
+        self.db = db
+        self.table = Table(name, db.metadata)
+        self.name = self.table.name
+
+        self.db.create_table(self.table, src, if_exists=if_exists)
 
 # class Table:
     # def __init__ (self, table, db):
@@ -81,8 +89,8 @@ if __name__ == "__main__":
 
     table = Table(table_name, source_db.metadata, autoload=True, autoload_with=source_db.engine)
     src_df = pd.read_sql_table(table.name, source_db.url)
-    s_table = dest_db.create_table("S", table, True)
-    m_table = dest_db.create_table("M", table, False)
+    s_table = ETL_Table("S", table, dest_db, True)
+    m_table = ETL_Table("M", table, dest_db, False)
     try:
         src_df.to_sql(s_table.name, dest_db.url, if_exists='append', index=False)
     except Exception as e:
@@ -93,17 +101,30 @@ if __name__ == "__main__":
     m_df = pd.read_sql_table(m_table.name, dest_db.url)
 
     # get x_table
+    x_table = ETL_Table("X", table, dest_db, True)
     x_df = pd.concat([s_df,m_df]).drop_duplicates(keep=False)
+    x_df.to_sql(x_table.name, dest_db.url, if_exists='append', index=False)
     # clean x_table
-    pk = str(s_table.primary_key.columns.values()[0].name)
+    # c_table remove duplicates
+    pk = str(s_table.table.primary_key.columns.values()[0].name)
     subset = list(s_df.columns)
     subset.remove(pk)
     c_df = x_df.drop_duplicates(subset=subset, keep=False)
-    c_df = c_df.fillna(-1)
-    c_df = c_df.replace("", "Missing")
-    print(c_df)
+    # c_table replace missing values
+    for col in c_df.columns:
+        null_value = "Unknown Value" if (c_df[col].dtypes == 'object') else -1
+        empty_value = "Missing Value" if (c_df[col].dtypes == 'object') else -2
+        c_df[col].fillna(null_value, inplace=True)
+        c_df[col].replace("", empty_value, inplace=True)
+    c_table = ETL_Table("C", table, dest_db, True)
+    c_df.to_sql(c_table.name, dest_db.url, if_exists='append', index=False)
+    # e_table get duplicates
     duplicates = x_df.duplicated(subset=subset, keep=False)
     e_df = x_df[duplicates]
+    e_table = ETL_Table("E", table, dest_db, True)
+    e_df.to_sql(e_table.name, dest_db.url, if_exists='append', index=False)
+    # m_table
+    c_df.to_sql(m_table.name, dest_db.url, if_exists='append', index=False)
     # e_table
     # c_table
     # table = Table(t, source_db)
